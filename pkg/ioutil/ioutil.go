01df99374d64843baf20dc5298f33de383496abf
Matthew Ziter <mziter@fastmail.com>
2022-12-01 09:32:42 -0500

    add pkg directory

---
+++ pkg/ioutil/ioutil.go
@@ -0,0 +1,44 @@
+package ioutil
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
+
+func GetLinesFromFile(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return lines
+}
+
+func GetLines(contents string) []string {
+	r := strings.NewReader(contents)
+
+	var lines []string
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return lines
+}
